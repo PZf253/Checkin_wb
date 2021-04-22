2b2ff3e7588c741803c3e0984bc6e561561a7802
PZf253 <82989017+PZf253@users.noreply.github.com>
2021-04-22 10:30:59 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,88 @@
+# settings
+import logging
+import json
+import requests
+import os
+
+from requests.exceptions import HTTPError
+
+__all__ = ['log', 'CONFIG', 'req']
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S')
+
+
+log = logger = logging
+
+
+class _Config:
+    CONTAINER_ID2 = '1008082b6009b92815bb8387156fc175b2c1c9'
+    SUPER_URL = 'https://m.weibo.cn/api/container/getIndex?containerid={}'.format('100803_-_page_my_follow_super')
+    WC_URL = 'https://m.weibo.cn/api/container/getIndex?containerid={}_-_feed'.format(CONTAINER_ID2)
+    KA_URL = 'https://ka.sina.com.cn/innerapi/draw'
+    BOX_URL = 'https://ka.sina.com.cn/html5/mybox'
+    WB_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E150'
+
+
+class ProductionConfig(_Config):
+    LOG_LEVEL = logging.INFO
+
+
+class DevelopmentConfig(_Config):
+    LOG_LEVEL = logging.DEBUG
+
+
+class HttpRequest(object):
+    @staticmethod
+    def to_python(json_str: str):
+        return json.loads(json_str)
+
+    @staticmethod
+    def to_json(obj):
+        return json.dumps(obj, indent=4, ensure_ascii=False)
+
+    def request(self, method, url, max_retry: int = 2,
+            params=None, data=None, json=None, headers=None, **kwargs):
+        for i in range(max_retry + 1):
+            try:
+                s = requests.Session()
+                response = s.request(method, url, params=params,
+                    data=data, json=json, headers=headers, **kwargs)
+            except HTTPError as e:
+                log.error(f'HTTP error:\n{e}')
+                log.error(f'The NO.{i + 1} request failed, retrying...')
+            except KeyError as e:
+                log.error(f'Wrong response:\n{e}')
+                log.error(f'The NO.{i + 1} request failed, retrying...')
+            except Exception as e:
+                log.error(f'Unknown error:\n{e}')
+                log.error(f'The NO.{i + 1} request failed, retrying...')
+            else:
+                return response
+
+        raise Exception(f'All {max_retry + 1} HTTP requests failed, die.')
+
+
+req = HttpRequest()
+
+RUN_ENV = os.environ.get('RUN_ENV', 'dev')
+if RUN_ENV == 'dev':
+    CONFIG = DevelopmentConfig()
+else:
+    CONFIG = ProductionConfig()
+
+log.basicConfig(level=CONFIG.LOG_LEVEL)
+
+
+MESSAGE_TEMPLATE = '''
+    {today:#^28}
+    🔅[{region_name}]{uid}
+    今日奖励: {award_name} × {award_cnt}
+    本月累签: {total_sign_day} 天
+    签到结果: {status}
+    {end:#^28}'''
+
+CONFIG.MESSAGE_TEMPLATE = MESSAGE_TEMPLATE
+
